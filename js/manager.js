f8a526411cd75d2702da2b471d00f64e4a7fa5f1
nadine-shi <nadine@afterlogic.com>
2016-08-16 16:23:42 +0300

    System - create checkUserRoleIsAtLeast method in Api

--- js/manager.js
+++ js/manager.js
@@ -19,7 +19,7 @@ module.exports = function (oAppData) {
 	
 	return {
 		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.PowerUser;
+			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
 		},
 		start: function (ModulesManager) {
 			ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/IcalAttachmentView.js'), 'BeforeMessageBody']);
