620c92d3de8654e58ea6ea411509cb058d5ffe2a
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:22:19 +0300

    System - use manager.js files instead of manager-smth.js

--- js/entry-pub.js
+++ js/entry-pub.js
@@ -5,13 +5,13 @@ var $ = require('jquery');
 $('body').ready(function () {
 	var
 		oAvaliableModules = {
-			'CalendarWebclient': require('modules/CalendarWebclient/js/manager-pub.js')
+			'CalendarWebclient': require('modules/CalendarWebclient/js/manager.js')
 		},
 		ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
 		App = require('%PathToCoreWebclientModule%/js/App.js')
 	;
 	
 	App.setPublic();
-	ModulesManager.init(oAvaliableModules, App.getUserRole(), App.isPublic());
+	ModulesManager.init(oAvaliableModules);
 	App.init();
 });
--- js/manager.js
+++ js/manager.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = function (oAppData) {
-	require('modules/%ModuleName%/js/koBindings.js');
 	require('modules/%ModuleName%/js/enums.js');
 	require('fullcalendar');
-	require('modules/%ModuleName%/js/MainTabExtMethods.js');
 
 	var
 		_ = require('underscore'),
 		
 		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+		
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
 		
 		Settings = require('modules/%ModuleName%/js/Settings.js'),
 		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
@@ -17,32 +17,66 @@ module.exports = function (oAppData) {
 	
 	Settings.init(oSettings);
 	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
-		},
-		start: function (ModulesManager) {
-			ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/IcalAttachmentView.js'), 'BeforeMessageBody']);
-			ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/CalendarSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
-		},
-		getScreens: function () {
-			var oScreens = {};
-			oScreens[Settings.HashModuleName] = function () {
-				return require('modules/%ModuleName%/js/views/CalendarView.js');
-			};
-			return oScreens;
-		},
-		getHeaderItem: function () {
+	if (App.isPublic())
+	{
+		return {
+			getScreens: function () {
+				var oScreens = {};
+				oScreens[Settings.HashModuleName] = function () {
+					return require('modules/%ModuleName%/js/views/CalendarView.js');
+				};
+				return oScreens;
+			},
+			getHeaderItem: function () {
+				return {
+					item: require('modules/%ModuleName%/js/views/PublicHeaderItem.js'),
+					name: Settings.HashModuleName
+				};
+			}
+		};
+	}
+	else if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		if (App.isNewTab())
+		{
 			return {
-				item: require('modules/%ModuleName%/js/views/HeaderItemView.js'),
-				name: Settings.HashModuleName
+				start: function (ModulesManager) {
+					ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/IcalAttachmentView.js'), 'BeforeMessageBody']);
+				}
 			};
-		},
-		getWeekStartsOn: function () {
-			return Settings.WeekStartsOn;
-		},
-		getMobileSyncSettingsView: function () {
-			return require('modules/%ModuleName%/js/views/MobileSyncSettingsView.js');
 		}
-	};
+		else
+		{
+			require('modules/%ModuleName%/js/koBindings.js');
+			require('modules/%ModuleName%/js/MainTabExtMethods.js');
+
+			return {
+				start: function (ModulesManager) {
+					ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/IcalAttachmentView.js'), 'BeforeMessageBody']);
+					ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/CalendarSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+				},
+				getScreens: function () {
+					var oScreens = {};
+					oScreens[Settings.HashModuleName] = function () {
+						return require('modules/%ModuleName%/js/views/CalendarView.js');
+					};
+					return oScreens;
+				},
+				getHeaderItem: function () {
+					return {
+						item: require('modules/%ModuleName%/js/views/HeaderItemView.js'),
+						name: Settings.HashModuleName
+					};
+				},
+				getWeekStartsOn: function () {
+					return Settings.WeekStartsOn;
+				},
+				getMobileSyncSettingsView: function () {
+					return require('modules/%ModuleName%/js/views/MobileSyncSettingsView.js');
+				}
+			};
+		}
+	}
+	
+	return null;
 };
